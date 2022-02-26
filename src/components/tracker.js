483c874f9e548178da5b7cf9ce6bfeb70d08eacc
Abhishek Jain <abhishek.j0407@gmail.com>
2022-02-26 18:07:38 +0530

    Comment component created

---
+++ src/components/tracker.js
@@ -0,0 +1,11 @@
+const tracker = () => {
+  return (
+    <div className="tracker">
+      <img src="../images/icon-plus.svg" alt="increment-track" className="tracker-icon" />
+      <h5 className="likes">12</h5>
+      <img src="../images/icon-minus.svg" alt="decrement-tracker" className="tracker-icon"/>
+    </div>
+  )
+}
+
+export default tracker
